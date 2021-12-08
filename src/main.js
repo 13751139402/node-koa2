6917ee60602ef71e756e1b782e2bb170a28a5e1f
bytedance <daixunwei@bytedance.com>
2021-12-08 14:32:22 +0800

    添加路由处理不同的URL请求

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ const { APP_PORT } = require("./config/config.default");
 
 const app = new Koa();
 
-app.use((ctx, next) => {
-  ctx.body = "hello world";
-});
+const Router = require("koa-router");
+const indexRouter = new Router();
+
+const userRouter = require("./router/user.route");
+app.use(userRouter.routes());
 
 app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`);
